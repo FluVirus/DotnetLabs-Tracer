cc0d8ee1d2fa7ba1b20da9368386f893ea9d91a6
Konanau Aliaksandr <m.kononov.alexander@gmail.com>
2022-09-20 17:50:25 +0300

    [Console Section] Implement primitive program to be traced
    It includes simple multithreading, recursion.

--- ConsoleSection/Program.cs
+++ ConsoleSection/Program.cs
@@ -1,9 +1,89 @@
-﻿namespace University.DotnetLabs.Lab1.ConsoleSection;
+﻿using University.DotnetLabs.Lab1.TracerLibrary;
+using University.DotnetLabs.Lab1.ConsoleSection.StreamWriters;
+using System.Xml;
+using System.Text.Json;
+
+namespace University.DotnetLabs.Lab1.ConsoleSection;
 
 class Program
 {
+    static private string XmlFilePath = "../../../../output.xml";
+    static private string JsonFilePath = "../../../../output.json"; 
+
+    static private int _x;
+    static private void Recursive()
+    {
+        _tracer.StartTrace();
+        for (int i = 0; i < 100; i++) 
+        {
+            Console.WriteLine("Hi From Recursive");
+        }
+        if (_x++ < 9) 
+        {
+            Recursive();
+        }
+        _tracer.StopTrace();
+    }
+    
+    static private readonly ITracer _tracer = new Tracer();
     static void Main(string[] args)
     {
-        
+        _tracer.StartTrace();
+        Thread thread = new Thread(() =>
+        {
+            _tracer.StartTrace();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("HI FROM ANOTHER THREAD");
+            }
+            _tracer.StopTrace();
+        });
+        Thread thread2 = new Thread(Recursive);
+        thread2.Start();
+        thread.Start();
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Hi from main thread");
+        }
+        thread.Join();
+        thread2.Join();
+        _tracer.StopTrace();
+
+        TraceResult result = _tracer.GetTraceResult();
+       
+        FileStream XmlFileStream = new FileStream(XmlFilePath, FileMode.OpenOrCreate);
+        FileStream JsonFileStream = new FileStream(JsonFilePath, FileMode.OpenOrCreate);
+        XmlWriterSettings settings = new XmlWriterSettings();
+        settings.ConformanceLevel = ConformanceLevel.Document;
+        settings.Indent = true;
+        ResultStreamWriter fXmlWriter = new ResultXmlWriter(XmlFileStream, settings);
+        ResultStreamWriter cXmlWriter = new ResultXmlWriter(Console.OpenStandardOutput(), settings);
+        JsonWriterOptions options = new JsonWriterOptions();
+        options.Indented = true;
+        ResultStreamWriter fJsonWriter = new ResultJsonWriter(JsonFileStream, options);
+        ResultStreamWriter cJsonWriter = new ResultJsonWriter(Console.OpenStandardOutput(), options);
+        using (fXmlWriter)
+        {
+            fXmlWriter.Write(result);
+        }
+        using (fJsonWriter)
+        {
+            fJsonWriter.Write(result);
+        }
+        Console.WriteLine();
+        Console.WriteLine("----------------------------XML-----------------------------");
+        using (cXmlWriter)
+        {
+            cXmlWriter.Write(result);
+        }
+        Console.WriteLine();
+        Console.WriteLine("----------------------------JSON-----------------------------");
+        using (cJsonWriter)
+        {
+            cJsonWriter.Write(result);
+        }
+        XmlFileStream.Dispose();
+        JsonFileStream.Dispose();
+        Console.ReadLine();
     }
 }
