a35ca1d40afe30d1e13d8275d0fe0f2e1ddd2ed9
Konanau Aliaksandr <m.kononov.alexander@gmail.com>
2022-09-20 10:09:46 +0300

    [Tracer Library] Add exception classes
    TracerStackException and TracerThreadException classes are added.
    The first one raised when call stack state is unexpected.
    The second one raised when thread in a dictionary cannot be found.

---
+++ TracerLibrary/Exceptions/TracerStackException.cs
@@ -0,0 +1,7 @@
+﻿namespace University.DotnetLabs.Lab1.TracerLibrary.Exceptions;
+
+public class TracerStackException : Exception
+{
+    public TracerStackException(string message) : base(message) {}
+    public TracerStackException() : base("Tracer Stack Exception raised") { }
+}
---
+++ TracerLibrary/Exceptions/TracerThreadException.cs
@@ -0,0 +1,7 @@
+﻿namespace University.DotnetLabs.Lab1.TracerLibrary.Exceptions;
+
+public class TracerThreadException : Exception
+{
+    public TracerThreadException(string message) : base(message) {}
+    public TracerThreadException() : base("Tracer Thread Exception raised") {}
+}
