7ec7e96b33789b39edbabe2ae32776c733526e5d
Konanau Aliaksandr <m.kononov.alexander@gmail.com>
2022-09-20 12:02:05 +0300

    [Tracer Library] Implement Tracer
    Class Tracer implements ITracer interface. It is based on concurrent dictionary
    of stacks for each thread. The stacks forms the tree of methods call, the root element belongs to
    class ThreadData. If unexpected state occurrs, the tracer throws 2 types of exceptions:
    TracerThreadException and TracerStackException.
    
    Returned result contains tracer internal references (may be it should be a clone?) - a
    collection of trees.

---
+++ TracerLibrary/Tracer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using University.DotnetLabs.Lab1.TracerLibrary.Exceptions;
+
+namespace University.DotnetLabs.Lab1.TracerLibrary;
+
+public class Tracer : ITracer
+{
+    private readonly ConcurrentDictionary<int, Stack<MethodTreeNode>> _stacks = new();
+    public TraceResult GetTraceResult()
+    {
+        LinkedList<ThreadData> threads = new();
+        foreach (Stack<MethodTreeNode> stack in _stacks.Values)
+        {
+            ThreadData threadData = (ThreadData)stack.First();
+            threads.AddLast(threadData);
+        }
+        return new TraceResult(threads);
+    }
+
+    public void StartTrace()
+    {
+        StackFrame stackFrame = new StackTrace().GetFrame(1); //get super method data
+        string methodName = stackFrame.GetMethod().Name;
+        string className = stackFrame.GetMethod().DeclaringType.FullName;
+        int threadID = Thread.CurrentThread.ManagedThreadId;
+        MethodData methodData = new MethodData(methodName, className);
+        Stack<MethodTreeNode> stack = _stacks.GetOrAdd(threadID, threadID =>
+        {
+            ThreadData threadData = new(threadID);
+            Stack<MethodTreeNode> newStack = new();
+            newStack.Push(threadData);
+            _stacks.TryAdd(threadID, newStack);
+            return newStack;
+        });
+        stack.Push(methodData);
+        methodData.Duration.Start();
+    }
+
+    public void StopTrace()
+    {
+        int threadID = Thread.CurrentThread.ManagedThreadId;
+        Stack<MethodTreeNode> stack;
+        bool successed = _stacks.TryGetValue(threadID, out stack);
+        if (!successed)
+        {
+            throw new TracerThreadException("There is no stack tracing with such id");
+        }
+        MethodTreeNode node;
+        successed = stack.TryPop(out node);
+        if (!successed || node is ThreadData)
+        {
+            throw new TracerStackException($"Stack with thread id {threadID} suddenly contains no data");
+        }
+        MethodData methodData = (MethodData)node;
+        methodData.Duration.Stop();
+        StackFrame stackFrame = new StackTrace().GetFrame(1);
+        string methodName = stackFrame.GetMethod().Name;
+        string className = stackFrame.GetMethod().DeclaringType.FullName;
+        if (!methodData.MethodName.Equals(methodName) || !methodData.ClassName.Equals(className))
+        {
+            throw new TracerStackException($"Unexpected StopTrace() in {className}.{methodName} . Expected {methodData.ClassName}.{methodData.MethodName} on stack.");
+        }
+        stack.Peek().InternalMethods.AddLast(methodData);
+        ThreadData threadData = (ThreadData) stack.First();
+        threadData.Duration = threadData.Duration.Add(methodData.Duration.Elapsed);
+    }
+}
