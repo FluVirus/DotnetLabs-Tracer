cee824192d9af712ec29d2658e817f2f905c56bc
Konanau Aliaksandr <m.kononov.alexander@gmail.com>
2022-09-20 19:06:08 +0300

    [Tracer Library] Add unit tests

---
+++ TracerLibrary.Test/TracerTests.cs
@@ -0,0 +1,49 @@
+using University.DotnetLabs.Lab1.TracerLibrary.Exceptions;
+namespace University.DotnetLabs.Lab1.TracerLibrary.Test;
+
+[TestClass]
+public class TracerTests
+{
+    private ITracer _tracer;
+    [TestInitialize]
+    public void Initialize()
+    {
+        _tracer = new Tracer();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(TracerStackException))]
+    public void TracerTest_TooManyStops()
+    {
+        _tracer.StartTrace();
+        int x = 5;
+        for (int i = 0; i < 10; i++)
+        {
+            x += x + 2;
+        }
+        _tracer.StopTrace();
+        _tracer.StopTrace();
+        _tracer.StopTrace();
+        TraceResult result = _tracer.GetTraceResult();
+    }
+    private void MethodStart() => _tracer.StartTrace();
+    private void MethodStop() => _tracer.StopTrace();
+
+    [TestMethod]
+    [ExpectedException(typeof(TracerStackException))]
+    public void TracerTest_DifferentStartAndStop()
+    {
+        MethodStart();
+        MethodStop();
+        TraceResult result = _tracer.GetTraceResult();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(TracerThreadException))]
+    public void TracerTest_SuddenStopWithoutStartInThread()
+    {
+        MethodStop();
+        TraceResult result = _tracer.GetTraceResult();
+    }
+
+}
